688b32568dea30c09e180105d71621376558fdda
PXLk21 <wsm566@gmail.com>
2025-06-14 21:21:03 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-from google_sheets import send_to_sheet
 
-data = {
-    "نوع العقار": "شقة",
-    "المدينة": "الرياض",
-    "السعر": "500000",
-    "جهة التواصل": "واتساب",
-    "رابط الإعلان": "https://example.com"
-}
-
-send_to_sheet(data)
